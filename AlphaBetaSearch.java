1c869ba7ca405507533131ebbb3f903602cd73cb
gislibg <gisligudmundss@gmail.com>
2017-02-27 15:55:10 +0000

    throw exception

--- AlphaBetaSearch.java
+++ AlphaBetaSearch.java
@@ -10,9 +10,9 @@ public class AlphaBetaSearch {
 	private boolean first = true;
 	ArrayList<Pair<Integer, Integer>> bestResult;
 	
-	AlphaBetaSearch(int cutoff)
+	AlphaBetaSearch()
 	{
-		this.cutoff = cutoff;
+		this.cutoff = 0;
 		bestResult = new ArrayList<Pair<Integer, Integer>>();
 		bestActionFrom = new Pair<Integer, Integer>(-1, -1);
 		bestActionTo = new Pair<Integer, Integer>(-1, -1);
@@ -25,45 +25,55 @@ public class AlphaBetaSearch {
 		int v = -1;
 		int alpha = 0;
 		int beta = 100;
-
+		
 		bestActionFrom = new Pair<Integer, Integer>(legalActions.get(0), legalActions.get(1));
 		bestActionTo = new Pair<Integer, Integer>(legalActions.get(2), legalActions.get(3));
-		for(int i = 0; i < legalActions.size(); i += 4)
+		
+		while(true)
 		{
-			Pair<Integer, Integer> checkActionFrom = new Pair<Integer, Integer>(legalActions.get(i), legalActions.get(i + 1));
-			Pair<Integer, Integer> checkActionTo = new Pair<Integer, Integer>(legalActions.get(i + 2 ), legalActions.get(i + 3));
-			
-			State temp = new State(state);		
-			temp.updateState(checkActionFrom, checkActionTo);
-
-
-			int fromMin = minValue(temp, alpha, beta, 0);
-			
-			if(fromMin > v)
+			try
 			{
-				v = fromMin;
-				alpha = Math.max(alpha, v);
-				bestActionFrom = new Pair<Integer, Integer>(legalActions.get(i), legalActions.get(i + 1));
-				bestActionTo = new Pair<Integer, Integer>(legalActions.get(i + 2), legalActions.get(i + 3));
-
-				if(alpha >= beta )
+				for(int i = 0; i < legalActions.size(); i += 4)
 				{
-					//System.out.println("best action found!" + legalActions.get(i) + " " + legalActions.get(i + 1) + " " + legalActions.get(i + 2) + legalActions.get(i + 3));
-					break;
+					Pair<Integer, Integer> checkActionFrom = new Pair<Integer, Integer>(legalActions.get(i), legalActions.get(i + 1));
+					Pair<Integer, Integer> checkActionTo = new Pair<Integer, Integer>(legalActions.get(i + 2 ), legalActions.get(i + 3));
+					
+					State temp = new State(state);		
+					temp.updateState(checkActionFrom, checkActionTo);
+
+
+					int fromMin = minValue(temp, alpha, beta, 0);
+					
+					if(fromMin > v)
+					{
+						v = fromMin;
+						alpha = Math.max(alpha, v);
+						bestActionFrom = new Pair<Integer, Integer>(legalActions.get(i), legalActions.get(i + 1));
+						bestActionTo = new Pair<Integer, Integer>(legalActions.get(i + 2), legalActions.get(i + 3));
+
+						if(alpha >= beta )
+						{
+							//System.out.println("best action found!" + legalActions.get(i) + " " + legalActions.get(i + 1) + " " + legalActions.get(i + 2) + legalActions.get(i + 3));
+							break;
+						}
+					}
 				}
 			}
+			catch(Exception x)
+			{
+				bestResult.add(bestActionFrom);
+				bestResult.add(bestActionTo);
+				break;
+			}
+			cutoff++;
 		}
-		
-		bestResult.add(bestActionFrom);
-		bestResult.add(bestActionTo);
-		
 		
 		return bestResult;
 	}
 	
 	private int minValue(State state, int alpha, int beta, int depth) {
 		
-		if(state.terminalTest() || depth >= cutoff)
+		if(state.terminalTest() || depth == cutoff)
 		{
 			int best = state.eval();
 			//System.out.println("BEST MOVE SAMKVAEMT EVAL() from min: " + Math.abs(best - depth) + "best is: " + best + " depth is: " + depth);
@@ -120,7 +130,6 @@ public class AlphaBetaSearch {
 		{	
 			int best = state.eval();
 			//System.out.println("BEST MOVE SAMKVAEMT EVAL() from max: " + Math.abs(best - depth) + "best is: " + best + " depth is: " + depth); 
-
 			return best;
 		}
 		
